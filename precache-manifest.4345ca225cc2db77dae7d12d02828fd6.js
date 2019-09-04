800c6b74cb8db9f7384e04e04326e8baf6e17800
kathrynjeanelizabethstacy@gmail.com <39036868+KJES4@users.noreply.github.com>
2019-09-03 23:03:42 -0400

    Updates

---
+++ precache-manifest.4345ca225cc2db77dae7d12d02828fd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6591492ea70aa4a820aa0ca600816083",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "6e30ad47338e4fb7c482",
+    "url": "/rolodex/static/css/main.2e6b1bfc.chunk.css"
+  },
+  {
+    "revision": "d2abed8e01ddc1ece2df",
+    "url": "/rolodex/static/js/2.f55c3fbc.chunk.js"
+  },
+  {
+    "revision": "6e30ad47338e4fb7c482",
+    "url": "/rolodex/static/js/main.96f5d216.chunk.js"
+  },
+  {
+    "revision": "5eaf27faf3e64b99dd18",
+    "url": "/rolodex/static/js/runtime~main.0e5d51cd.js"
+  },
+  {
+    "revision": "0e57bce241b171fa79f57a7499c29d67",
+    "url": "/rolodex/static/media/mainLogo.0e57bce2.svg"
+  }
+]);
